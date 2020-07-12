ae10e75526a02eee4afe7c836e339a647f0fcfa2
fmemoraveis <67616471+fmemoraveis@users.noreply.github.com>
2020-07-12 09:12:23 -0300

    introdução do modelo de ML no arquivo python

--- prob_furto.py
+++ prob_furto.py
@@ -9,14 +9,23 @@ from sklearn.neighbors import NearestNeighbors
 import warnings
 warnings.filterwarnings("ignore")
 
+import pickle
+
+# load model
+model = pickle.load(open('model.pkl','rb'))
+
 # apresentar números com 3 casas decimais
-pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.float_format', lambda x: '%.0f' % x)
 
 
 # Text/Title
 st.title("Probabilidade de furto do seu veículo")
 
-localizacao = st.text_input("Localização do Veiculo / Endereço","Digite a localização..")
+hora_leit = st.number_input(label="Hora da leitura")
+periodo_leit = st.number_input(label="Periodo da leitura")
+hora_gprs = st.number_input(label="Hora da gprs")
+periodo_gprs = st.number_input(label="Periodo da gprs")
+peso = st.number_input(label="Peso")
 
 
 all_models = ['HB20','ONIX','GOL','Saveiro']
@@ -24,7 +33,18 @@ model_choice = st.selectbox("Modelo",all_models)
 
 ano = st.text_input("Ano de fabricação do veículo")
 
+df_prob = pd.DataFrame({'hora_leit': hora_leit,
+                        'periodo_leit': periodo_leit,
+                        'hora_gprs': hora_gprs,
+                        'periodo_gprs': periodo_gprs, 
+                        'peso': peso,
+                        'temp' : [0] })
+
+del df_prob['temp']
+
+df_prob.head()
 
 if st.button("Submeter"):
-    st.success('Rodar modelo preditivo')
+    probability_class_1 = model.predict_proba(df_prob)[0, 1]
+    st.write(probability_class_1*100)  
 	
